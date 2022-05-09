06056a3ad5fa468c7ef42b45069eb5dd33845083
中村　迅 <shu.kyu0224@icloud.com>
2022-05-10 00:27:41 +0900

    月別リスト、モーダル下書き

---
+++ src/components/AttendanceTable.vue
@@ -0,0 +1,34 @@
+<style lang="scss" scoped>
+  @import '@/styles/AttendanceTable.scss';
+</style>
+
+<template>
+  <div class="list">
+    <table>
+      <tr>
+        <th class="list__ttl-day">日付</th>
+        <th class="list__ttl-start">出勤時間</th>
+        <th class="list__ttl-end">退勤時間</th>
+        <th class="list__ttl-breake">休憩時間</th>
+        <th class="list__ttl-remarks">備考</th>
+        <th class="list__ttl-edit">編集</th>
+      </tr>
+      <tr>
+        <td class="list__day">1日</td>
+        <td class="list__start">9:00</td>
+        <td class="list__end">18:00</td>
+        <td class="list__breake">1:00</td>
+        <td class="list__remarks">テストテスト。。。</td>
+        <td class="list__edit"><button><font-awesome-icon icon="fa-solid fa-pen" /></button></td>
+      </tr>
+      <tr>
+        <td class="list__day">2日</td>
+        <td class="list__start">9:00</td>
+        <td class="list__end">18:00</td>
+        <td class="list__breake">1:00</td>
+        <td class="list__remarks">テストテスト。。。</td>
+        <td class="list__edit"><button><font-awesome-icon icon="fa-solid fa-pen" /></button></td>
+      </tr>
+    </table>
+  </div>
+</template>
---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<style lang="scss" scoped>
+  @import '@/styles/Modal.scss';
+</style>
+
+<template>
+  <div class="modal">
+    <div class="modal__card">
+      <div class="modal__head">
+        <h3 class="modal__today">5/9(月)</h3>
+        <button><font-awesome-icon icon="fa-solid fa-xmark" /></button>
+      </div>
+      <div class="modal__time">
+        <div class="modal__start">
+          <input type="text" placeholder="9:00">
+          <button class="modal__set-time">打刻</button>
+        </div>
+        <div class="modal__end">
+          <input type="text" placeholder="18:00">
+          <button class="modal__set-time">打刻</button>
+        </div>
+        <div class="modal__breake">
+          <input type="text" placeholder="1:00">
+        </div>
+      </div>
+      <div class="modal__business">
+        <textarea class="modal__content" placeholder="業務内容"></textarea>
+      </div>
+      <div class="modal__register">
+        <button>登録</button>
+      </div>
+    </div>
+  </div>
+</template>
